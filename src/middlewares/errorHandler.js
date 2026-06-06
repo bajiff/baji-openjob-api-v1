269bf75c68f1b426225492e9083cbdcc1408767f
BajiLinux <mr.baji.smart@gmail.com>
2026-06-06 20:41:49 +0700

    feat(src): beresin isi folder src

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,19 @@
+import ClientError from '../exceptions/ClientError.js';
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof ClientError) {
+    return res.status(err.statusCode).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+
+  // Error dari server (500)
+  console.error(err);
+  return res.status(500).json({
+    status: 'error',
+    message: 'Maaf, terjadi kegagalan pada server kami.',
+  });
+};
+
+export default errorHandler;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import process from 'process';
 import express from 'express';
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 const port = process.env.PORT;
@@ -8,9 +9,12 @@ const host = process.env.HOST;
 
 app.use(express.json());
 
+// Endpoint test
 app.get('/', (req, res) => {
     res.send({ message: 'OpenJob RESTful API V1 is running with ES Modules!' });
 });
+
+app.use(errorHandler);
 
 app.listen(port, host, () => {
     console.log(`Server berjalan pada http://${host}:${port}`);
